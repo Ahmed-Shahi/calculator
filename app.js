16751582f8b542fcb8bad066b7ac9f83b6895b3c
Ahmed-Shahi <ahmedshahi038@gmail.com>
2023-10-04 21:49:22 +0500

    not done

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
+var a=document.getElementById('inpf')
+
 function function1(z){
-    document.getElementById('inpf').value += z
+    a.value += z
 }
 
 function function2(){
-    var a=document.getElementById('inpf')
     a.value = eval(a.value)
 }
 
 function function3(){
-    document.getElementById('inpf').value = ''
+    a.value = ''
 }
 function function5(){
-    var u =document.getElementById('inpf')
-    u.value = u.value.slice(0, -1)
+    a.value = a.value.slice(0, -1)
 }
